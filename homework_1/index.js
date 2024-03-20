fde76ca2a7eefdaa4178afd14a20f52c51bd3a04
Svyryd <svirids1@yahoo.com>
2024-03-20 23:36:04 +0200

    added put and post

--- homework_1/index.js
+++ homework_1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Масив з якого можемо додавати або видаляти елементи
@@ -20,20 +21,37 @@ app.get('/items/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const item = items.find((item) => item.id === id);
     if (!item) {
-        res.status(404).send('Item not found');
+        res.status(404).send('Не знайдено');
         return;
     }
     res.json(item);
 });
 
-// POST
+// POST -- Додати елемент в масив
 app.post('/items', (req, res) => {
-    res.json('Server post');
+    console.log('Додаємо:', req.body);
+    const newItem = req.body;
+    if (!newItem || !newItem.id || !newItem.name) {
+        res.status(400).send('Невірно вказаний формат name або id');
+        return;
+    }
+    items.push(newItem);
+    res.status(201).send('Дані введено коректно');
 });
 
 // PUT
 app.put('/items/:id', (req, res) => {
-    res.json('Server put');
+    console.log('Запит на зміну:', req.body);
+    const id = parseInt(req.params.id);
+    const putItem = req.body;
+
+    const foundItem = items.findIndex((item) => item.id === id);
+    if (foundItem === -1) {
+        res.status(400).send('Не знайдено елемент за id');
+        return;
+    }
+    items[foundItem] = { ...items[foundItem], ...putItem };
+    res.json(items[foundItem]);
 });
 
 // DELETE -- Видалити елемент
